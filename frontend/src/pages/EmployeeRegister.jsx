a8e4529f6274f966a9ec213b02ac564989830ce1
Clevon Wong <clevonwongjy@gmail.com>
2022-07-06 16:23:06 +0800

    added basic useEffects

--- frontend/src/pages/EmployeeRegister.jsx
+++ frontend/src/pages/EmployeeRegister.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "@mui/system";
 import { Stack } from "@mui/material";
 import { register, reset } from "../features/auth/authSlice";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FormInput from "../components/formComponents/FormInput";
 
 const EmployeeRegister = () => {
   const { handleSubmit, control } = useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const [snackbarState, setSnackbarState] = useState({
     open: false,
     vertical: "top",
     horizontal: "center",
-    message: "",
+    snackbarMessage: "",
   });
 
-  const { vertical, horizontal, open, message } = snackbarState;
+  const { vertical, horizontal, open, snackbarMessage } = snackbarState;
 
   const openSnackbar = (newState) => {
     setSnackbarState({
@@ -40,12 +42,28 @@ const EmployeeRegister = () => {
     setSnackbarState({ ...snackbarState, open: false });
   };
 
+  useEffect(() => {
+    if (isError) {
+      openSnackbar({
+        vertical: "bottom",
+        horizontal: "center",
+        snackbarMessage: message,
+      });
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onSubmit = (data) => {
     if (data.password !== data.password2) {
       openSnackbar({
         vertical: "bottom",
         horizontal: "center",
-        message: "Passwords do not match",
+        snackbarMessage: "Passwords do not match",
       });
     } else {
       data["role"] = "employee";
@@ -67,7 +85,7 @@ const EmployeeRegister = () => {
         anchorOrigin={{ vertical, horizontal }}
         open={open}
         onClose={handleClose}
-        message={message}
+        message={snackbarMessage}
         key={vertical + horizontal}
       />
       <form onSubmit={handleSubmit(onSubmit)}>
